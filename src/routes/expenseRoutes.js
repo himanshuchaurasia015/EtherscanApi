4a305776656008d6da0d9e0993cb5b46d6a58846
Himanshu V Chaurasia <himanshuchaurasia015@gmail.com>
2024-09-01 01:46:22 +0530

    Route to calculate expense created

--- src/routes/expenseRoutes.js
+++ src/routes/expenseRoutes.js
@@ -2,6 +2,6 @@ const express = require("express");
 const { calcExpense } = require("../controllers/expenseController");
 const router = express.Router();
 
-router.get("/expense", calcExpense);
+router.get("/calculate", calcExpense);
 
 module.exports = router;
